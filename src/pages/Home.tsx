740d3f03b7b792c6c47f9a6b9dac546c4a3a647d
Y3ppi3 <89306551+Y3ppi3@users.noreply.github.com>
2025-02-15 00:52:16 +0400

    beta v.0.0.0.1
    first beta

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="text-center">
+            <h1 className="text-3xl font-bold text-blue-600">Главная страница</h1>
+            <p>Добро пожаловать в MathLingo!</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,10 @@
+function Login() {
+    return (
+        <div className="text-center">
+            <h1 className="text-3xl font-bold text-green-600">Вход</h1>
+            <p>Введите логин и пароль.</p>
+        </div>
+    );
+}
+
+export default Login;
