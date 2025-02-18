e65b6e2c6de7fdf0cb0ad3163ff8264a09fb9778
Y3ppi3 <89306551+Y3ppi3@users.noreply.github.com>
2025-02-18 20:37:56 +0400

    0

--- mathlingo-frontend/src/components/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const ProtectedRoute = () => {
-    const token = localStorage.getItem("token"); // Получаем токен из localStorage
-
-    return token ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
