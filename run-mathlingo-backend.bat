b98401435d08221d2e2e637b4a6d9c08a816572e
Y3ppi3 <89306551+Y3ppi3@users.noreply.github.com>
2025-04-12 00:06:03 +0400

    v.0.0.1.6.2b

---
+++ run-mathlingo-backend.bat
@@ -0,0 +1,16 @@
+@echo off
+rem Скрипт для запуска только API Gateway
+
+rem Создаем директорию для логов
+if not exist "logs" mkdir logs
+
+echo =========================================================
+echo    Запуск Mathlingo backend    
+echo =========================================================
+
+cd mathlingo-backend
+echo Запуск Mathlingo backend на порту 8000...
+docker compose up
+
+rem Команда выше не вернется, пока пользователь не нажмет Ctrl+C
+echo API Gateway остановлен.
